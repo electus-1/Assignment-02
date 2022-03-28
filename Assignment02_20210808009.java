e3ef7482fcab1b1cd1182e6b8a539c172c0e3ae0
Baran Kırca <barankircaa@gmail.com>
2022-03-29 00:39:21 +0300

    Added AccountNotFoundException

--- Assignment02_20210808009.java
+++ Assignment02_20210808009.java
@@ -1,5 +1,26 @@
+/*
+ * @author: Baran Kirca
+ * @version: 29.03.2022
+ */
+
+import java.lang.RuntimeException;
+import java.util.ArrayList;
+
 public class Assignment02_20210808009 {
     public static void main(String[] args) {
 
+    }
+}
+
+class AccountNotFoundException extends RuntimeException {
+    private String acctNum;
+
+    public AccountNotFoundException(String acctNum) {
+        this.acctNum = acctNum;
+    }
+
+    @Override
+    public String toString() {
+        return "AccountNotFoundException: " + this.acctNum;
     }
 }
