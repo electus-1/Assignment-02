4f957ea323d7894a6af571e583e021408e796ef5
Baran Kırca <barankircaa@gmail.com>
2022-03-29 01:51:35 +0300

    Added account also re-added constructors for exceptions

--- Assignment02_20210808009.java
+++ Assignment02_20210808009.java
@@ -8,12 +8,69 @@ import java.util.ArrayList;
 
 public class Assignment02_20210808009 {
     public static void main(String[] args) {
+        
+    }
+}
+
+class Account {
+    private String accountNumber;
+    private double balance;
+
+    Account() {
 
     }
+
+    Account(String accountNumber) {
+        this.accountNumber = accountNumber;
+        this.balance = 0.0;
+
+    }
+
+    Account(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        if (balance < 0) {
+            this.balance = 0.0;
+        } else {
+            this.balance = balance;
+        }
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount >= 0) {
+            this.balance += amount;
+        } else {
+            throw new InvalidAmountException(amount);
+        }
+    }
+
+    public void withdrawal(double amount) {
+        if (amount >= 0 && this.balance >= amount) {
+            this.balance -= amount;
+        } else {
+            throw new InvalidAmountException(amount);
+        }
+    }
+
+    public String toString() {
+        return "Account " + getAccountNumber() + " has " + getBalance();
+    }
+
 }
 
 class AccountNotFoundException extends RuntimeException {
     private String acctNum;
+
+    public AccountNotFoundException(String acctNum) {
+        this.acctNum = acctNum;
+    }
 
     @Override
     public String toString() {
@@ -23,6 +80,10 @@ class AccountNotFoundException extends RuntimeException {
 
 class BalanceRemainingException extends RuntimeException {
     private double balance;
+
+    public BalanceRemainingException(double balance) {
+        this.balance = balance;
+    }
 
     public double getBalance() {
         return this.balance;
@@ -37,6 +98,11 @@ class BalanceRemainingException extends RuntimeException {
 class CompanyNotFoundException extends RuntimeException {
     private int id;
     private String name;
+
+    public CompanyNotFoundException(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     @Override
     public String toString() {
@@ -55,6 +121,12 @@ class CustomerNotFoundException extends RuntimeException {
     private String name;
     private String surname;
 
+    public CustomerNotFoundException(int id, String name, String surname) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+    }
+
     @Override
     public String toString() {
         if (this.id == 0) {
@@ -70,6 +142,10 @@ class CustomerNotFoundException extends RuntimeException {
 
 class InvalidAmountException extends RuntimeException {
     private double amount;
+
+    public InvalidAmountException(double amount) {
+        this.amount = amount;
+    }
 
     @Override
     public String toString() {
